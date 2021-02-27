603cd349f4b0608e120b2e836069064b0cf62e60
Jesse Callahan <jessetylercallahan@gmail.com>
2021-02-26 20:24:59 -0800

    adds order display function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Business Logic
+
 function OrdersList() {
   this.orders = []
 }
@@ -26,29 +28,45 @@ Pizza.prototype.priceAdd = function () {
   return this.price = toppingsPrice + sizePrice
 }
 
+//User Interface
 
-
+function displayOrderDetails(pizzaToDisplay) {
+  var orderList = $("ul#output");
+  var htmlForpizzaInfo = "";
+  pizzaToDisplay.orders.forEach(function (pizza) {
+    htmlForpizzaInfo += "<li id=" + pizza.id + ">" + pizza.name + " " + pizza.size + " size pizza" + " $" + pizza.price + " Toppings: " + pizza.toppings + " ";
+  });
+  orderList.html(htmlForpizzaInfo);
+};
 
 $(document).ready(function () {
+  //start fake db
   let newOrdersList = new OrdersList()
 
+  //pizza order form 
   $("form#new-order").submit(function (event) {
 
     let newPizza = new Pizza()
     event.preventDefault();
+
     newPizza.name = $("input#new-name").val();
     newPizza.size = $("input:radio[name=size]:checked").val()
     newPizza.toppings = [];
     $("input:checkbox[name=toppings]:checked").each(function () { newPizza.toppings.push($(this).val()); });
+
     newPizza.priceAdd()
     newOrdersList.addId(newPizza)
     newOrdersList.pushToOrdersList(newPizza)
-    console.log(newPizza)
 
-    // $('input[type=checkbox]').prop('checked', false);
+    //displays order 
+    displayOrderDetails(newOrdersList)
+
+    //clears form
+    $('input[type=checkbox]').prop('checked', false);
 
 
   });
-  console.log(newOrdersList)
+
+
 
 });
